f0f73af158417879d0ff413202f356ae19e1d248
Lex-Smith <e.d.smith@mail.ru>
2024-02-29 21:23:43 +0300

    class Runner refactored, corrected UtilizationFactory

--- src/main/java/com/javaacademy/Runner.java
+++ src/main/java/com/javaacademy/Runner.java
@@ -3,6 +3,7 @@ package com.javaacademy;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import lombok.Cleanup;
 
 /**
  * Runner
@@ -15,8 +16,7 @@ public class Runner {
     public static void main(String[] args) throws IOException, GarbageNotRefactorableException {
         City paris = new City("Paris", 10_000_000);
         Garbage[] garbageArray = paris.createGarbage();
-        try (BufferedWriter journal = new BufferedWriter(new FileWriter("journal.txt"))) {
-            UtilizationFactory.refactorGarbage(garbageArray, journal);
-        }
+        @Cleanup BufferedWriter journal = new BufferedWriter(new FileWriter("journal.txt"));
+        UtilizationFactory.refactorGarbage(garbageArray, journal);
     }
 }
