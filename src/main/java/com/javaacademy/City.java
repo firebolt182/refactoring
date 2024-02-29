69fac11234ee44118403b249d0a86e8b50670135
Hero777 <clickker@mail.ru>
2024-02-29 16:23:11 +0100

    рефакторинг класса City

--- src/main/java/com/javaacademy/City.java
+++ src/main/java/com/javaacademy/City.java
@@ -1,45 +1,27 @@
 package com.javaacademy;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 /**
  * Город
  */
-public class City {
-    public final String name;
-    public int peopleCount;
 
-    public City(String name, Integer peopleCount) {
-        if (name == null) {
-            throw new NullPointerException("name is marked non-null, but is null");
-        }
-        this.name = name;
-        this.peopleCount = peopleCount;
-    }
+@Getter
+@Setter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString(exclude = {"peopleCount"})
+public class City {
+    @NonNull
+    final String name;
+    int peopleCount;
 
     public Garbage[] createGarbage() {
-        return new Garbage[] {
-            new Garbage(GarbageType.GLASS, 1000, this.name),
-            new Garbage(GarbageType.PAPER, 15000, this.name),
-            new Garbage(GarbageType.MIXED, 200000, this.name),
+        return new Garbage[]{
+                new Garbage(GarbageType.GLASS, 1000, this.name),
+                new Garbage(GarbageType.PAPER, 15000, this.name),
+                new Garbage(GarbageType.MIXED, 200000, this.name),
         };
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getPeopleCount() {
-        return peopleCount;
-    }
-
-    public void setPeopleCount(Integer peopleCount) {
-        this.peopleCount = peopleCount;
-    }
-
-    //Внимание, только город выводится на экран
-    @Override
-    public String toString() {
-        return "City{" +
-                "name='" + name + '\'' +
-                '}';
     }
 }
